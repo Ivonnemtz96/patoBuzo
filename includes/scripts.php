56240be623e23a6dd2b0dfdb0e33d383ea7c6d59
Ivonne Mtz <54880593+Ivonnemtz96@users.noreply.github.com>
2023-12-22 17:09:22 -0700

    nuevos cambios

--- includes/scripts.php
+++ includes/scripts.php
@@ -1,8 +1,7 @@
-<script src="assets/js/vendor/jquery.min.js"></script>
-<script src="assets/js/vendor/jquery-migrate-3.0.0.min.js"></script>
-<script src="assets/js/app.min.js"></script>
-<script src="assets/js/vscustom-carousel.min.js"></script>
-<script src="assets/js/vsmenu.min.js"></script>
-<script src="assets/js/ajax-mail.js"></script>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDC3Ip9iVC0nIxC6V14CKLQ1HZNF_65qEQ"></script>
-<script src="assets/js/main.js"></script>
+<script src="assets/js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="assets/js/app.min.js"></script>
+    <script src="assets/js/vscustom-carousel.min.js"></script>
+    <script src="assets/js/vsmenu.min.js"></script>
+    <script src="assets/js/ajax-mail.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDC3Ip9iVC0nIxC6V14CKLQ1HZNF_65qEQ"></script>
+    <script src="assets/js/main.js"></script>
