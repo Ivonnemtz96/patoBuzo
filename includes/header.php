dd1b9a1f9e2c923cd65f4189fd34de4d8cd0ca5a
Ivonne Mtz <54880593+Ivonnemtz96@users.noreply.github.com>
2023-12-22 17:04:24 -0700

    nievo proyecto, no se ven los estilos

---
+++ includes/header.php
@@ -0,0 +1,89 @@
+<div class="popup-search-box d-none d-lg-block">
+    <button class="searchClose border-theme text-theme">
+        <i class="fal fa-times"></i>
+    </button>
+    <form action="#">
+        <input type="text" class="border-theme" placeholder="What are you looking for" />
+        <button type="submit"><i class="fal fa-search"></i></button>
+    </form>
+</div>
+<div class="sticky-header-wrap sticky-header py-1 py-sm-2 py-lg-1">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-5 col-md-3">
+                <div class="logo">
+                    <a href="index.html"><img src="assets/img/logo.svg" alt="logo" /></a>
+                </div>
+            </div>
+            <div class="col-7 col-md-9 text-right">
+                <nav class="main-menu menu-sticky1 onepage-nav d-none d-lg-block link-inherit">
+                    <ul>
+                        <li><a href="index.html">Home</a></li>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#food-menu">Menu</a></li>
+                        <li><a href="#chef">Chef</a></li>
+                        <li><a href="#gallery">Gallery</a></li>
+                        <li><a href="#blog">Blog</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
+                </nav>
+                <button class="vs-menu-toggle text-theme border-theme d-inline-block d-lg-none">
+                    <i class="far fa-bars"></i>
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="vs-menu-wrapper">
+    <div class="vs-menu-area">
+        <button class="vs-menu-toggle text-theme">
+            <i class="fal fa-times"></i>
+        </button>
+        <div class="mobile-logo">
+            <a href="index.html"><img src="assets/img/logo.svg" alt="logo" /></a>
+        </div>
+        <div class="vs-mobile-menu onepage-nav link-inherit"></div>
+    </div>
+</div>
+<header class="header-wrapper header-layout2 bg-dark dark-style py-2 py-lg-0 px-xl-115">
+    <div class="container-fluid">
+        <div class="row align-items-center">
+            <div class="col-6 col-md-3 col-lg-2 col-xl-2">
+                <div class="header-logo">
+                    <a href="index.html"><img src="assets/img/logo-white.svg" alt="Logo" /></a>
+                </div>
+            </div>
+            <div class="col-6 col-md-9 col-lg-7 col-xl-5">
+                <nav class="main-menu menu-style1 onepage-nav link-inherit text-right text-xl-left mobile-menu-active">
+                    <ul>
+                        <li><a href="index.html">Home</a></li>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#food-menu">Menu</a></li>
+                        <li><a href="#chef">Chef</a></li>
+                        <li><a href="#gallery">Gallery</a></li>
+                        <li><a href="#blog">Blog</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
+                </nav>
+                <button type="button" class="vs-menu-toggle ml-auto d-block text-theme border-theme d-lg-none">
+                    <i class="far fa-bars"></i>
+                </button>
+            </div>
+            <div class="col-lg-3 col-xl-5">
+                <div class="header-right d-none d-lg-flex align-items-center justify-content-end">
+                    <div class="contact-info media align-items-center d-none d-xl-flex">
+                        <div class="media-icon mr-15 pl-30" data-overlay="theme" data-opacity="1">
+                            <i class="fal fa-clock text-theme fa-2x"></i>
+                        </div>
+                        <div class="media-body">
+                            <span class="d-block mb-1 text-white-light">Call for Order</span>
+                            <p class="mb-0 h4 text-font1">
+                                <a href="tel:+869526212">+869 526 212</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
